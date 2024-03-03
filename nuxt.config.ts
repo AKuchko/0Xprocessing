e0497a945a4665ec698541c07780744df9552ac5
AKuchko <vasco_da_gama1469@mail.ru>
2024-03-03 23:31:36 +0700

    feat(assets): ubuntu fonts, scss variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxtjs/google-fonts',
+  ],
+
+  googleFonts: {
+    families: {
+      Ubuntu: [400, 700],
+    },
+    preload: true,
+    download: true,
+    outputDir: 'assets',
+  }
 })
